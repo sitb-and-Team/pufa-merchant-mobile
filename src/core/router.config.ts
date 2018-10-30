d99f77334130e564461f109756a9db3fa64e89eb
yangyao <873241789@qq.com>
2018-10-30 14:57:22 +0800

    添加交易记录 模块

--- src/core/router.config.ts
+++ src/core/router.config.ts
@@ -1,6 +1,10 @@
 import { Home } from '../container/Home';
+import { Trade } from '../container/Trade';
 
 export default [{
   path: '/home',
   component: Home,
+}, {
+  path: '/trade',
+  component: Trade,
 }];
