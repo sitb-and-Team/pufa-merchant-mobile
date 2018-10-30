3b217e783db982ef053a79fb48446557135de192
yangyao <873241789@qq.com>
2018-10-30 17:32:13 +0800

    add 常量file

---
+++ src/locale/zh-cn.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/10/30
+ */
+export const lang = {
+
+};
+
+
+export const menu = {
+  home: '首页',
+  user: '我'
+};
+
+export const resErrMsg = {};
