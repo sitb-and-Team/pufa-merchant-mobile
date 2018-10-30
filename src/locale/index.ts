0bdcbe74b5a318e1d29d7ec1cc628713393e8d93
yangyao <873241789@qq.com>
2018-10-30 17:32:27 +0800

    add 常量file

---
+++ src/locale/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/10/30
+ */
+import { lang, menu, resErrMsg } from './zh-cn';
+
+export { lang, menu, resErrMsg };
