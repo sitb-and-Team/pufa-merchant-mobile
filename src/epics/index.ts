5c02041f027c8561fa6449eff4622ff2fc3ba046
yangyao <873241789@qq.com>
2018-10-30 14:54:29 +0800

    添加 trade action

--- src/epics/index.ts
+++ src/epics/index.ts
@@ -1,11 +1,13 @@
 import * as navigator from './navigator';
 import * as root from './root';
+import * as trade from './trade';
 
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/4
  */
 export {
+  trade,
   navigator,
   root
 }
