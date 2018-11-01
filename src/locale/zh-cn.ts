b2a23fcfa8927353ede7b4821df9b45ff12324be
yangyao <873241789@qq.com>
2018-11-01 19:30:03 +0800

    新增 费率支付

--- src/locale/zh-cn.ts
+++ src/locale/zh-cn.ts
@@ -4,6 +4,9 @@
  * date: 2018/10/30
  */
 export const lang = {
+  aliPay: '支付宝支付',
+  weChat: '微信支付',
+  quickPay: '快捷支付',
   address: '地址',
   confirm: '确认',
   cancel: '取消',
@@ -13,6 +16,16 @@ export const lang = {
   legalEmail: '法人邮箱',
   idType: '证件类型',
   idNo: '证件号',
+  normalFeeRate: {
+    fee: '费率',
+    type: '费率类型',
+    min: '最低费率'
+  },
+  serviceFeeRate: {
+    fee: '服务费率',
+    type: '服务费类型',
+    min: '最低服务费'
+  },
   settleBankName: '结算银行名称',
   settleBankNo: '结算银行行号',
   accountName: '结算账户名',
@@ -33,7 +46,8 @@ export const menu = {
   user: '我',
   tradeRecord: '交易记录',
   merchantBinding: '商户绑定',
-  merchantInfo: '商户信息'
+  merchantInfo: '商户信息',
+  merchantRate: '费率信息'
 };
 
 export const resErrMsg = {};
