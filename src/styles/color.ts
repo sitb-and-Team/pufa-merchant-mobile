f7471b1c8541c945b26545ff1a93852154fc7fe8
yangyao <873241789@qq.com>
2018-11-01 19:32:50 +0800

    添加 背景颜色定义

---
+++ src/styles/color.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/11/1
+ */
+  // 背景颜色
+export const background = {
+    weChat: '#62b900',
+    aliPay: '#009fe9',
+    quickPay: '#f98e09',
+    default: '#e25400'
+  };
