214dfaf8d4c871d5f5368eef681fa0e883a37e57
yangyao <873241789@qq.com>
2018-11-01 10:42:42 +0800

    新增商户模块部分的字段

--- src/locale/zh-cn.ts
+++ src/locale/zh-cn.ts
@@ -4,8 +4,19 @@
  * date: 2018/10/30
  */
 export const lang = {
+  address: '地址',
   confirm: '确认',
   cancel: '取消',
+  legalPerson: '法人姓名',
+  legalPersonPhone: '法人电话',
+  legalIdNo: '法人身份证号码',
+  legalEmail: '法人邮箱',
+  idType: '证件类型',
+  idNo: '证件号',
+  settleBankName: '结算银行名称',
+  settleBankNo: '结算银行行号',
+  accountName: '结算账户名',
+  accountNumber: '结算账户号',
   merchantName:'商户名',
   merchantNo: '商户号',
   checkValue: '验证码',
