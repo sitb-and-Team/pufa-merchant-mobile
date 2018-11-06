17e4db5fdbab1ea98be4a347288778fc5843cdb2
yangyao <873241789@qq.com>
2018-11-06 14:36:37 +0800

    错误代码

---
+++ src/constants/ErrorCode.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/11/6
+ */
+export const ErrorCode: any = {
+  SUCCESS: '0000',
+  EXCEPTION: '3000',
+  CUSTOM_ERROR: '3200',
+  UNAUTHORIZED: '1000'
+};
