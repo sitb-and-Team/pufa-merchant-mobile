f4cb3f280279bc71a64afe68337af91a2ce2a868
yangyao <873241789@qq.com>
2018-11-06 14:32:50 +0800

    发起 token请求

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Redirect, Route } from 'react-router-dom';
+import { queryToObjectWithUrl } from '@sitb/wbs/utils/HttpUtil';
+
 import { getActions, getState } from '../core/store';
+import { setAccessToken } from '../core/SessionServices';
 import routes from '../core/router.config';
 
 function createRender(route) {
@@ -22,7 +25,20 @@ function createRender(route) {
 export default class App extends React.Component<any, any> {
 
   componentWillMount() {
-    getActions().root.start();
+    // 获取当前链接url，判断是否有token
+    let nowHref = location.href;
+    if (nowHref.indexOf('?') > -1) {
+      // 获取当前token，返回search object
+      const {access_token} = queryToObjectWithUrl(nowHref);
+      if (access_token) {
+        // session 保存access token
+        setAccessToken(access_token);
+        // 截取掉url？之后掉链接 并跳转
+        location.href = nowHref.substr(0, nowHref.indexOf('?'));
+      }
+    }
+    // 加载当前用户信息
+    getActions().session.startProfile();
   }
 
   render() {
