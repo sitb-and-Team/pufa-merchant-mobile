88392bd79c80eb6034a6aa47fd2eac42b935f5b8
yangyao <873241789@qq.com>
2018-11-06 14:36:28 +0800

    新增clear reducer

--- src/reducers/root.ts
+++ src/reducers/root.ts
@@ -1,5 +1,6 @@
 import compose from './composeReducer';
 import { root } from '../constants/ActionTypes';
+import { resetStorage } from '../core/SessionServices';
 
 const DEFAULT_STATE = {};
 
@@ -7,6 +8,12 @@ export default compose((state = DEFAULT_STATE, {type}) => {
   switch (type) {
     case root.start:
       console.log('start reducers');
+      return {
+        ...state
+      };
+    case 'clear':
+      // 清空浏览器缓存
+      resetStorage();
       return {
         ...state
       };
