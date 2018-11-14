0a61637e38588da8f9fe940e30b955f2b82db1d0
yangyao <873241789@qq.com>
2018-11-14 19:07:13 +0800

    更换 模块名称

--- src/core/router.config.ts
+++ src/core/router.config.ts
@@ -1,16 +1,20 @@
-import { App } from '../container/Application/App';
-import { Trade } from '../container/Trade';
-import { Binding } from '../container/Merchant/Binding';
-import { MerchantInfo } from '../container/Merchant/MerchantInfo';
 import { menu } from '../locale';
-import { MerchantRate } from '../container/Merchant/MerchantRate';
+
+import { App } from '../container/Application/App';
+
 import { PaymentDetail } from '../container/Payment/Payment';
+import { Trade } from '../container/Trade';
+import { MerchantLogin } from '../container/Merchant/Login';
+import { MerchantBinding } from '../container/Merchant/Binding';
+import { MerchantInfo } from '../container/Merchant/MerchantInfo';
+import { MerchantRate } from '../container/Merchant/MerchantRate';
 
 // 路由列表
 export const routerPath = {
   app: '/app',
   trade: '/trade',
   merchantBinding: '/binding',
+  merchantLogin: '/login',
   merchantInfo: '/merchantInfo',
   merchantRate: '/merchantRate',
   paymentDetail: '/paymentDetail'
@@ -25,8 +29,12 @@ export default [{
   title: menu.tradeRecord
 }, {
   path: routerPath.merchantBinding,
-  component: Binding,
+  component: MerchantBinding,
   title: menu.merchantBinding
+}, {
+  path: routerPath.merchantLogin,
+  component: MerchantLogin,
+  title: menu.merchantLogin
 }, {
   path: routerPath.merchantInfo,
   component: MerchantInfo,
