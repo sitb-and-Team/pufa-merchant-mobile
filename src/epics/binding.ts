601b8d23d292b4ba2ebcc07f7ede85430fe94963
yangyao <873241789@qq.com>
2018-11-14 19:10:34 +0800

    发送验证码 绑定成功跳转 弹出消息提示

--- src/epics/binding.ts
+++ src/epics/binding.ts
@@ -1,12 +1,13 @@
-import {ofType} from 'redux-observable';
-import {binding as types} from '../constants/ActionTypes';
-import {mergeMap, switchMap} from "rxjs/operators";
-import {execute} from "../core/Request";
+import { ofType } from 'redux-observable';
+import { binding as types } from '../constants/ActionTypes';
+import { mergeMap, switchMap } from "rxjs/operators";
+import { execute } from "../core/Request";
 import URL from "../constants/URL";
-import {urlArgs} from "@sitb/wbs/utils/HttpUtil";
-import {merge, of} from 'rxjs/index';
-import {getActions} from '../core/store';
-import {routerPath} from "../core/router.config";
+import { urlArgs } from "@sitb/wbs/utils/HttpUtil";
+import { merge, of } from 'rxjs/index';
+import { getActions, getState } from '../core/store';
+import { map, tap } from 'rxjs/internal/operators';
+import actionToast from '../core/actionToast';
 
 /**
  * 绑定用户
@@ -15,7 +16,7 @@ import {routerPath} from "../core/router.config";
 export function startQuery(action$) {
   return action$.pipe(
     ofType(types.startQuery),
-    switchMap(({payload}) => {
+    switchMap(({payload}: any) => {
       let newPayload = Object.assign({}, payload);
       const {merchantNo, checkValue} = newPayload;
       return execute({
@@ -24,6 +25,7 @@ export function startQuery(action$) {
         body: JSON.stringify({checkValue}),
       })
     }),
+    tap(actionToast()),
     mergeMap((payload: any) => {
       const {success} = payload;
       const result = [of({
@@ -32,7 +34,8 @@ export function startQuery(action$) {
       })];
       // 成功发起query请求
       if (success) {
-        getActions().navigator.navigate(routerPath.app);
+        // 重置验证码、获取商户信息
+        getActions().binding.countDownComplete();
         getActions().session.startProfile();
       }
       return merge(...result);
@@ -47,9 +50,32 @@ export function startQuery(action$) {
 export function sendVerificationCode(action$) {
   return action$.pipe(
     ofType(types.sendVerificationCode),
-    switchMap(({payload}) => execute({
-      url: `${URL.bindSend}?${urlArgs(payload)}`,
-      type: types.sendVerificationCodeComplete
-    })))
+    switchMap(({payload}: any) => execute({
+      url: `${URL.bindSend}?${urlArgs(payload)}`
+    })),
+    tap(actionToast()),
+    map(({success}) => {
+      if (success) {
+        // 重置验证码为60秒
+        getActions().binding.resetCountDown();
+        let timer = setInterval(() => {
+          // 获取倒计时
+          let countDown = getState().binding.countDown;
+
+          // 为0时清空定时器
+          if (countDown === 0) {
+            clearInterval(timer);
+            getActions().binding.countDownComplete();
+            return;
+          }
+          // 读秒
+          getActions().binding.startCountDown(countDown - 1);
+        }, 1000);
+      }
+      return ({
+        type: types.sendVerificationCodeComplete
+      });
+    })
+  )
 }
 
