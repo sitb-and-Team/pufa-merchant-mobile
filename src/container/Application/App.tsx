6e18ead3d84f1eb61c49eea946b39b762613217f
HJF <2283785225@qq.com>
2018-11-19 20:13:05 +0800

    首页title显示改为用户名

--- src/container/Application/App.tsx
+++ src/container/Application/App.tsx
@@ -19,6 +19,7 @@ import {getActions} from '../../core/store';
 import {User} from './User';
 import {Home} from './Home';
 import {menu} from '../../locale';
+import {getMerchantId, getOperator} from "../../core/SessionServices";
 
 const styles: any = theme => ({
   tabs: {
@@ -55,10 +56,13 @@ class Container extends React.Component<any, any> {
   };
 
   render() {
+    const loginMerchant = getOperator() && getOperator().find(merchant => merchant.merchantNo === getMerchantId()) || {};
+    const {merchantName} = loginMerchant;
     const {match, location, classes} = this.props;
+    console.log(this.props);
     // 导航路由配置
     const routes = [{
-      path: `${match.url}/Home`,
+      path: `${match.url}/${merchantName}`,
       render: this.createRender(Home, menu.home),
       label: menu.home,
       Icon: HomeIcon
