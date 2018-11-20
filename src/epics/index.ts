15b1f15922cfb39408f504b651864b427276dd36
HJF <2283785225@qq.com>
2018-11-20 12:42:05 +0800

    二维码ActionType index

--- src/epics/index.ts
+++ src/epics/index.ts
@@ -4,6 +4,7 @@ import * as trade from './trade';
 import * as session from './session';
 import * as binding from './binding';
 import * as payment from './payment';
+import * as qrCode from './qrcode';
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/4
@@ -14,5 +15,6 @@ export {
   root,
   session,
   binding,
-  payment
+  payment,
+  qrCode
 }
