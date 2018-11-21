892979d29cc04f80afd23ff37b54ebb612cdeb2f
HJF <2283785225@qq.com>
2018-11-21 10:11:23 +0800

    交易页面传值

--- src/epics/paymentDetail.ts
+++ src/epics/paymentDetail.ts
@@ -1,6 +1,6 @@
 import URL from '../constants/URL';
 import {ofType} from 'redux-observable';
-import { switchMap} from 'rxjs/operators';
+import {map, switchMap} from 'rxjs/operators';
 import {payment as types} from '../constants/ActionTypes';
 import {execute} from '../core/Request';
 import {urlArgs} from "@sitb/wbs/utils/HttpUtil";
@@ -13,11 +13,35 @@ export function searchPaymentTrade(action$) {
   return action$.pipe(
     ofType(types.searchPaymentTrade),
     switchMap(({payload}: any) => {
-      const {merchantNo, ...other} = payload;
-      console.log(other);
+      const {merchantNo, isHome, ...other} = payload;
       return execute({
         url: `${URL.payment}/trades/${merchantNo}?${urlArgs(other)}`,
-        type: types.searchPaymentTradeComplete
+        inputValue: isHome
+      })
+    }),
+    map((payload) => {
+      return {
+        type: types.searchPaymentTradeComplete,
+        payload: {
+          ...payload
+        }
+      }
+    })
+  )
+}
+
+/**
+ * 查看交易信息
+ * @param action$
+ */
+export function searchAppPayment(action$) {
+  return action$.pipe(
+    ofType(types.searchAppPayment),
+    switchMap(({payload}: any) => {
+      const {merchantNo, ...other} = payload;
+      return execute({
+        url: `${URL.payment}/trades/${merchantNo}?${urlArgs(other)}`,
+        type: types.searchAppPaymentComplete
       })
     }))
 }
