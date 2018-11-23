c317feb30748eff90598171670fed1833d30e13f
HJF <2283785225@qq.com>
2018-11-23 15:25:06 +0800

    更改 action 名字

--- src/epics/index.ts
+++ src/epics/index.ts
@@ -1,15 +1,13 @@
 import * as navigator from './navigator';
 import * as root from './root';
-import * as trade from './trade';
 import * as session from './session';
 import * as binding from './binding';
-import * as payment from './paymentDetail';
+import * as payment from './payment';
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/4
  */
 export {
-  trade,
   navigator,
   root,
   session,
