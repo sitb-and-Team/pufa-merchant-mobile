15ea24f3fb6d60b7e94ec876aee4a711c9a7cf40
HJF <2283785225@qq.com>
2018-11-24 11:51:02 +0800

    优化config 生成

--- src/container/Merchant/MerchantRate.tsx
+++ src/container/Merchant/MerchantRate.tsx
@@ -6,127 +6,105 @@
 import * as React from 'react';
 
 import {withStyles} from '@material-ui/core/styles';
-import {lang} from '../../locale';
 import {SitbCard} from '../../component/Card';
 import weChat from '@sitb/svg-icon/weChat';
 import aliPay from '@sitb/svg-icon/aliPay';
 import quickPay from '@sitb/svg-icon/quickPay';
 import mposPay from '@sitb/svg-icon/mpos';
+import bankCard from '@sitb/svg-icon/bankCard';
 import {connect} from "react-redux";
 import {background} from "../../styles/color";
-import {getMerchantId, getOperator} from "../../core/SessionServices";
+import {getMerchantId, getOperator, nowOperator} from "../../core/SessionServices";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ColumnUtil from "../../utils/ColumnUtil";
+import {rateTemplate} from "./fields";
+import {BusinessTypeData} from "../../constants/BusinessType";
 
 // css
 const styles: any = theme => ({});
+
+const checkWord = ['debit', 'ownerDebit', 'credit', 'ownerCredit', 'abroadDebit', 'abroadCredit'];
 
 @connect(({session}) => ({
   hasLogin: session.hasLogin,
   agencies: session.agencies
 }))
-
 class Container extends React.Component<any, any> {
+
+  someBusinessType(type) {
+    return checkWord.some(string => string === type);
+  }
+
   render() {
-    const loginMerchant = getOperator() && getOperator().find(merchant => merchant.merchantNo === getMerchantId()) || {};
+    const loginMerchant = nowOperator(getOperator(), getMerchantId());
     const configs: any = [];
 
     const businesses = loginMerchant && loginMerchant.businesses || [];
+
     // 匹配出当前loginMerchantBusiness
     businesses.map((business) => {
-      let Default_business: any = {};
+      let Default_business: any = [];
+      let titleIconProps: any = {width: 30, height: 30};
+      let titleIcon: any = (props) => bankCard(props);
       /**
        * 公有
        */
-      const basic = [{
-        label: lang.serviceFeeRate.fee,
-        value: 'serviceFeeRate.fee',
-        setValue: rate => rate && (business.serviceFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.serviceFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.serviceFeeRate.min,
-        value: 'serviceFeeRate.min',
-        setValue: rate => rate && (business.serviceFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.serviceFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.serviceFeeRate.max,
-        value: 'serviceFeeRate.max',
-        setValue: rate => rate && (business.serviceFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.serviceFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.normalFeeRate.fee,
-        value: 'normalFeeRate.fee',
-        setValue: rate => rate && (business.normalFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.normalFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.normalFeeRate.min,
-        value: 'normalFeeRate.min',
-        setValue: rate => rate && (business.normalFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.normalFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.normalFeeRate.max,
-        value: 'normalFeeRate.max',
-        setValue: rate => rate && (business.normalFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.normalFeeRate.type)}` || `${rate}元`)
-      }];
-      /**
-       * POS独有
-       */
-      const posRate = [{
-        label: lang.creditFeeRate.fee,
-        value: 'creditFeeRate.fee',
-        setValue: rate => rate && (business.creditFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.creditFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.creditFeeRate.min,
-        value: 'creditFeeRate.min',
-        setValue: rate => rate && (business.creditFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.creditFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.creditFeeRate.max,
-        value: 'creditFeeRate.max',
-        setValue: rate => rate && (business.creditFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.creditFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.debitFeeRate.fee,
-        value: 'debitFeeRate.fee',
-        setValue: rate => rate && (business.debitFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.debitFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.debitFeeRate.min,
-        value: 'debitFeeRate.min',
-        setValue: rate => rate && (business.debitFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.debitFeeRate.type)}` || `${rate}元`)
-      }, {
-        label: lang.debitFeeRate.max,
-        value: 'debitFeeRate.max',
-        setValue: rate => rate && (business.debitFeeRate.type && `${rate}${ColumnUtil.unitJudgment(business.debitFeeRate.type)}` || `${rate}元`)
-      }];
-      if (business.businessType === "WeChat_PAY") {
-        Default_business = {
-          title: lang.weChat,
-          titleIcon: weChat({fill: background.weChat, width: 30, height: 30}),
-          ActionIcon: ExpandMoreIcon,
-          config: basic
+      const basicConfig: any = [];
+      // pos
+      const posConfig: any = [];
+      // 判断费率
+      if (business.normalFeeRate) {
+        basicConfig.push(...rateTemplate('normalFeeRate'));
+      }
+      // 判断服务费
+      if (business.serviceFeeRate) {
+        basicConfig.push(...rateTemplate('serviceFeeRate'));
+      }
+      // 判断是否有pos
+      if (business.posFeeRate) {
+        Object.keys(business.posFeeRate).forEach(type => {
+          if (this.someBusinessType(type)) {
+            posConfig.push(...rateTemplate(type));
+          }
+        });
+        basicConfig.push(...posConfig);
+      }
+      if (business.businessType.search('WeChat') !== -1) {
+        titleIcon = weChat;
+        titleIconProps = {
+          ...titleIconProps,
+          fill: background.weChat
+        }
+      }
+      if (business.businessType.search('AliPay') !== -1) {
+        titleIcon = aliPay;
+        titleIconProps = {
+          ...titleIconProps,
+          fill: background.aliPay
+        }
+      }
+      if (business.businessType.search('UNION') !== -1) {
+        titleIcon = quickPay;
+        titleIconProps = {
+          ...titleIconProps,
+          fill: background.quickPay
+        }
+      }
+      if (business.businessType.search('POS') !== -1) {
+        titleIcon = mposPay;
+        titleIconProps = {
+          ...titleIconProps,
+          fill: background.default
         }
       }
 
-      if (business.businessType === "AliPay_PAY") {
-        Default_business = {
-          title: lang.aliPay,
-          titleIcon: aliPay({fill: background.aliPay, width: 30, height: 30}),
-          ActionIcon: ExpandMoreIcon,
-          config: basic
-        }
-      }
+      Default_business.push({
+        title: BusinessTypeData[business.businessType],
+        titleIcon: titleIcon(titleIconProps),
+        ActionIcon: ExpandMoreIcon,
+        config: basicConfig
+      });
 
-      if (business.businessType === "UNION_PAY") {
-        Default_business = {
-          title: lang.quickPay,
-          titleIcon: quickPay({fill: background.quickPay, width: 30, height: 30}),
-          ActionIcon: ExpandMoreIcon,
-          config: basic
-        }
-      }
-      if (business.businessType === "POS_PAY") {
-        basic.push(...posRate);
-        Default_business = {
-          title: lang.mpos,
-          titleIcon: mposPay({fill: background.default, width: 30, height: 30}),
-          ActionIcon: ExpandMoreIcon,
-          config: basic
-        }
-      }
-      configs.push(Default_business);
+      configs.push(...Default_business);
     });
     return (
       <React.Fragment>
