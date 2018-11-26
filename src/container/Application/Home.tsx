9acc1e01f68134b97eaf31b361fe4d5f8bf09516
HJF <2283785225@qq.com>
2018-11-26 17:03:14 +0800

    优化

--- src/container/Application/Home.tsx
+++ src/container/Application/Home.tsx
@@ -184,10 +184,10 @@ class Container extends React.Component<any> {
         <ListItemText primary={`${totalAmount} 元`}
                       style={{minWidth: 80}}
                       secondary={
-                        <div>
+                        <span>
                           <span style={{width: 10, height: 10, backgroundColor: tradeStatusColorOptions[item.status], display: 'inline-block', borderRadius: '50%'}} />
                           <span>{tradeStatusOptions[item.status]}</span>
-                        </div>
+                        </span>
                       }
         />
       </ListItem>
