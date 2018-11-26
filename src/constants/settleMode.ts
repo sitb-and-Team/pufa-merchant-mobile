9c38203179600972785de915f54c0341d93c7b90
HJF <2283785225@qq.com>
2018-11-26 11:29:56 +0800

    结算模式映射值

---
+++ src/constants/settleMode.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: HJF(2283785225@qq.com)
+ * date: 2018/11/26
+ */
+/**
+ * 结算模式
+ */
+import { lang } from '../locale/index';
+
+// 结算模式select
+export const settleModeOptions = {
+  DIFFERENCE: lang.mode.difference,
+  FULL_AMOUNT: lang.mode.fullAmount
+};
