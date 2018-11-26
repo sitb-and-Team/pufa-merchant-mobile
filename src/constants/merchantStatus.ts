3e4f0f67d1b6e57fdfa7dbbd566b874787725c23
HJF <2283785225@qq.com>
2018-11-26 11:31:08 +0800

    商户状态映射值

---
+++ src/constants/merchantStatus.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: HJF(2283785225@qq.com)
+ * date: 2018/11/26
+ */
+/**
+ * 商户状态
+ */
+import { lang } from '../locale/index';
+
+// 商户状态select
+export const merchantStatusOptions = {
+  NORMAL: lang.merchant.status.normal,
+  WAIT_ACTIVE: lang.merchant.status.waitActive,
+  DISABLED: lang.merchant.status.disabled,
+  DELETE: lang.merchant.status.delete,
+};
