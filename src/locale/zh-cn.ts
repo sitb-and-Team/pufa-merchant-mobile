aa9a33300174f543457c64b32d20ebedd92bed62
HJF <2283785225@qq.com>
2018-11-26 17:07:07 +0800

    删除部分无用的交易状态

--- src/locale/zh-cn.ts
+++ src/locale/zh-cn.ts
@@ -118,9 +118,6 @@ export const lang = {
     failure: "交易失败",
     waitPayment: "待支付",
     processing: "处理中",
-    cancelled: "交易撤销",
-    refunded: "交易已经退货",
-    refundedPart: "已经部分退货",
     close: "交易关闭",
     delete: "删除"
   },
