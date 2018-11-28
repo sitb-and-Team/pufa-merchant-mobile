30db7cd41f959aabd35b7b84ae5cd15af770b42b
HJF <2283785225@qq.com>
2018-11-28 09:36:32 +0800

    颜色预显示文件 待定

---
+++ src/constants/RateType.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: HJF(2283785225@qq.com)
+ * date: 2018/11/27
+ */
+
+// import {lang} from "../locale";
+
+/**
+ * 费率类型
+ */
+
+/*export const rateType = {
+  default:'',
+  normalFeeRate: lang.normalFeeRate,
+  serviceFeeRate: lang.serviceFeeRate,
+  debit: lang.posFeeRate.debit,
+  ownerDebit: lang.posFeeRate.ownerDebit,
+  credit: lang.posFeeRate.credit,
+  ownerCredit: lang.posFeeRate.ownerCredit,
+  abroadDebit: lang.posFeeRate.abroadDebit,
+  abroadCredit: lang.posFeeRate.abroadCredit
+}*/
+
+// 费率类型color
+export const rateTypeColorOptions = {
+  default:'#777',
+  normalFeeRate: '#2b2',
+  serviceFeeRate: '#fb1122',
+  debit: '#f90',
+  ownerDebit: '#0bb',
+  credit: '#888',
+  ownerCredit: '#888',
+  abroadDebit: '#888',
+  abroadCredit: '#888'
+};
