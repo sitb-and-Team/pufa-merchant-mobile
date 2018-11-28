92a30f70520fcc8460ff20e730b6a178c8cb69f0
HJF <2283785225@qq.com>
2018-11-28 23:40:24 +0800

    首页统计信息显示

--- src/container/Application/Home.tsx
+++ src/container/Application/Home.tsx
@@ -5,15 +5,12 @@
  */
 import * as React from 'react';
 import {connect} from 'react-redux';
-import classNames from 'classnames';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Tab from '@material-ui/core/Tab';
-import grey from '@material-ui/core/colors/grey';
-import List from '@material-ui/core/List';
+// import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
@@ -21,10 +18,15 @@ import {routerPath} from '../../core/router.config';
 import {getActions} from '../../core/store';
 import {autoBind} from "@sitb/wbs/autoBind";
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import MoneyOffRoundedIcon from '@material-ui/icons/MoneyOffRounded';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import {menu} from "../../locale";
-import ListSubheader from "@material-ui/core/es/ListSubheader/ListSubheader";
+import weChat from "@sitb/svg-icon/weChat";
+import aliPay from "@sitb/svg-icon/aliPay";
+import quickPay from "@sitb/svg-icon/quickPay";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import {background} from "../../styles/color";
+import money from "@sitb/svg-icon/money";
+import {List} from "veigar/List/List";
 
 // css
 const styles: any = theme => ({
@@ -33,18 +35,8 @@ const styles: any = theme => ({
     paddingBottom: 40
   },
   card: {
-    height: 120,
+    height: 100,
     fontSize: 16
-  },
-  header_mode: {
-    backgroundColor: theme.palette.primary.main
-  },
-  headerTitle_mode: {
-    color: grey[200]
-  },
-  headerMoney_mode: {
-    fontSize: 40,
-    color: '#fff'
   },
   right: {
     textAlign: 'right'
@@ -54,18 +46,6 @@ const styles: any = theme => ({
     paddingBottom: 72
   }
 });
-
-function TabContainer(props) {
-  const {children, ...other} = props;
-  return (
-    <Typography component="div"
-                color="textSecondary"
-                {...other}
-    >
-      {children}
-    </Typography>
-  );
-}
 
 @connect(({payment}) => ({
   searchParams: payment.searchParams,
@@ -83,6 +63,7 @@ class Container extends React.Component<any> {
 
   componentWillMount() {
     this.handleSearch({page: 0});
+    getActions().payment.searchStats();
   }
 
   /**
@@ -124,8 +105,54 @@ class Container extends React.Component<any> {
     })
   }
 
+  /**
+   * 渲染list
+   * @param {any} item  当前行数据
+   * @param {any} index 下标
+   * @returns {any}
+   */
+  renderItem({item, index}) {
+    const {businessType} = item;
+    let type = businessType;
+    // svg默认props
+    let svgProps = {fill: '#fff', width: 30, height: 30};
+    let svg = {
+      'weChat': weChat,
+      'aliPay': aliPay,
+      'quickPay': quickPay
+    };
+    if (businessType.search("We") !== -1) {
+      type = "weChat";
+    }
+    if (businessType.search("Ali") !== -1) {
+      type = "aliPay";
+    }
+    if (businessType.search("UNION") !== -1) {
+      type = "quickPay";
+    }
+
+    item.totalAmount = parseFloat(item.totalAmount).toFixed(2);
+
+    return (
+      <ListItem key={index}
+                divider={index % 5 === 0}
+      >
+        <Avatar style={{background: background[type] || background.default}}>
+          {svg[type] && svg[type](svgProps) || money(svgProps)}
+        </Avatar>
+        <ListItemText primary='交易笔数：'
+                      secondary='交易总额：'
+        />
+        <ListItemText primary={`${item.totalElements} 笔`}
+                      secondary={`${item.totalAmount} 元`}
+                      style={{textAlign: 'right', padding: 0}}/>
+      </ListItem>
+    )
+  }
+
   render() {
-    const {classes, page} = this.props;
+    const {classes, page, homePage} = this.props;
+    console.log(homePage);
     page.value = page.value ? parseFloat(page.value).toFixed(2) : '0.00';
     // tab配置
     const config = [{
@@ -136,22 +163,15 @@ class Container extends React.Component<any> {
       label: menu.enterAccount,
       Icon: AccountBalanceWalletIcon,
       path: routerPath.enterAccount
-    }, {
-      label: menu.outAccount,
-      Icon: MoneyOffRoundedIcon,
-      path: routerPath.outAccount
     }];
     return (
       <Grid>
-        <Grid item
-              xs={12}
-              className={classNames(classes.header, classes.header_mode)}
-        >
-          <TabContainer align="center">
-            <p className={classes.headerTitle_mode}>{'总收入(元)'}</p>
-            <span className={classes.headerMoney_mode}>{page.value}</span>
-          </TabContainer>
-        </Grid>
+        <List data={homePage.content}
+              renderItem={this.renderItem}
+              onEndReachedThreshold={50}
+              className={classes.list}
+              useBodyScroll
+        />
         <Card className={classes.card}>
           <CardContent>
             <Grid container
@@ -163,16 +183,6 @@ class Container extends React.Component<any> {
             </Grid>
           </CardContent>
         </Card>
-        <List subheader={<ListSubheader component="div">统计信息</ListSubheader>}>
-          <ListItem>
-            <ListItemText primary="总笔数: "/>
-            <ListItemText primary={`${page.totalElements} 笔`} className={classes.right}/>
-          </ListItem>
-          <ListItem>
-            <ListItemText primary='总金额: '/>
-            <ListItemText primary={`${page.value} 元`} className={classes.right}/>
-          </ListItem>
-        </List>
       </Grid>
     )
   }
