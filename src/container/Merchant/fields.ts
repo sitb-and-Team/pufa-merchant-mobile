409e1a50f6b595e78d999c5a809e510224e21248
HJF <2283785225@qq.com>
2018-11-28 09:35:32 +0800

    颜色 显示

--- src/container/Merchant/fields.ts
+++ src/container/Merchant/fields.ts
@@ -12,14 +12,17 @@ export const rateTemplate: any = (prefixString) => [{
   setValue: (rate, dataResource) => {
     const type = objectPath.get(dataResource, `${prefixString}.type`);
     return rate && (type && `${rate}${ColumnUtil.unitJudgment(type)}` || `${parseFloat(rate).toFixed(2)}元`)
-  }
+  },
+  color: ''
 }, {
   label: objectPath.get(lang, `${prefixString}.min`),
   value: `${prefixString}.min`,
-  setValue: min => min && `${parseFloat(min).toFixed(2)}元`
+  setValue: min => min && `${parseFloat(min).toFixed(2)}元`,
+  color: ''
 }, {
   label: objectPath.get(lang, `${prefixString}.max`),
   value: `${prefixString}.max`,
-  setValue: max => max && `${parseFloat(max).toFixed(2)}元`
+  setValue: max => max && `${parseFloat(max).toFixed(2)}元`,
+  color: ''
 }];
 
