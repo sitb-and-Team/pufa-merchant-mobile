21cf79b21cf790396b4f59422c8fff007546abbe
HJF <2283785225@qq.com>
2018-11-29 15:54:26 +0800

    删除/ me 请求

--- src/container/Merchant/index.tsx
+++ src/container/Merchant/index.tsx
@@ -7,17 +7,11 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { MerchantBinding } from './Binding';
 import { MerchantLogin } from './Login';
-import { getActions } from '../../core/store';
 
 @connect(({session}) => ({
   hasBinding: session.hasBinding
 }))
 export class MerchantIndex extends React.Component<any> {
-
-  componentWillMount(){
-    // 加载当前用户信息
-    getActions().session.startProfile();
-  }
 
   render() {
     const {hasBinding} = this.props;
