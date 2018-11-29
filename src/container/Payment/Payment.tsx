5c02901a9b1d2014e6e3ce579f92363076fe4edd
HJF <2283785225@qq.com>
2018-11-29 23:07:42 +0800

    交易详情 显示更改

--- src/container/Payment/Payment.tsx
+++ src/container/Payment/Payment.tsx
@@ -6,6 +6,9 @@
 import * as React from 'react';
 import moment from 'moment';
 import {withStyles} from '@material-ui/core/styles';
+import classNames from 'classnames';
+import accounting from 'accounting';
+
 import {lang} from "../../locale";
 import {SitbCard} from "../../component/Card";
 import {getActions} from "../../core/store";
@@ -13,10 +16,45 @@ import {routerPath} from "../../core/router.config";
 import {tradeStatusOptions} from "../../constants/tradeStatus";
 import {BusinessTypeColor, BusinessTypeData} from "../../constants/BusinessType";
 import {momentCommon} from "../../constants/objectKey";
+import Grid from "@material-ui/core/Grid/Grid";
+import Typography from "@material-ui/core/es/Typography/Typography";
+import {grey} from "@material-ui/core/es/colors";
 
 
 // css
-const styles = theme => ({});
+const styles = theme => ({
+  header: {
+    paddingTop: 20,
+    paddingBottom: 15
+  },
+  header_mode: {
+    backgroundColor: theme.palette.primary.main
+  },
+  headerTitle_mode: {
+    color: grey[200],
+    fontSize: 12
+  },
+  headerMoney_mode: {
+    fontSize: 40,
+    color: '#fff'
+  },
+  headerMoney_second: {
+    fontSize: 18,
+    color: '#fff'
+  }
+});
+
+function TabContainer(props) {
+  const {children, ...other} = props;
+  return (
+    <Typography component="div"
+                color="textSecondary"
+                {...other}
+    >
+      {children}
+    </Typography>
+  );
+}
 
 class Container extends React.Component<any, any> {
 
@@ -29,7 +67,7 @@ class Container extends React.Component<any, any> {
   }
 
   render() {
-    const {params} = this.props;
+    const {params, classes} = this.props;
     if (params.paymentAt) {
       params.paymentAt = params && `${moment(params.paymentAt).format(momentCommon.DATETIME_FORMAT)}` || '';
     }
@@ -88,9 +126,20 @@ class Container extends React.Component<any, any> {
       config: basic
     }];
     return (
-      <SitbCard configs={configs}
-                dataResource={params}
-      />
+      <Grid>
+        <Grid item
+              xs={12}
+              className={classNames(classes.header, classes.header_mode)}
+        >
+          <TabContainer align="center">
+            <p className={classes.headerTitle_mode}>{'交易金额(元)'}</p>
+            <span className={classes.headerMoney_mode}>{accounting.formatMoney(params.totalAmount, '', 2)}</span>
+          </TabContainer>
+        </Grid>
+        <SitbCard configs={configs}
+                  dataResource={params}
+        />
+      </Grid>
     )
   }
 }
