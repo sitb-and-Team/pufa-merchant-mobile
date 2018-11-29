5dcff20c6c46a27ab87f77057b82325f5cec8088
HJF <2283785225@qq.com>
2018-11-29 21:12:32 +0800

    交易详情页面时间显示

--- src/container/Payment/Payment.tsx
+++ src/container/Payment/Payment.tsx
@@ -4,6 +4,7 @@
  * date: 2018/11/12
  */
 import * as React from 'react';
+import moment from 'moment';
 import {withStyles} from '@material-ui/core/styles';
 import {lang} from "../../locale";
 import {SitbCard} from "../../component/Card";
@@ -11,6 +12,7 @@ import {getActions} from "../../core/store";
 import {routerPath} from "../../core/router.config";
 import {tradeStatusOptions} from "../../constants/tradeStatus";
 import {BusinessTypeData} from "../../constants/BusinessType";
+import {momentCommon} from "../../constants/objectKey";
 
 
 // css
@@ -28,6 +30,11 @@ class Container extends React.Component<any, any> {
 
   render() {
     const {params} = this.props;
+    if (params.paymentAt) {
+      params.paymentAt = params && `${moment(params.paymentAt).format(momentCommon.DATETIME_FORMAT)}` || '';
+    }
+    console.log(params.paymentAt);
+    params.createAt = params && `${moment(params.createAt).format(momentCommon.DATETIME_FORMAT)}` || '';
     params.totalAmount = parseFloat(params.totalAmount).toFixed(2);
     const basic = [{
       label: lang.merchantNo,
@@ -50,6 +57,9 @@ class Container extends React.Component<any, any> {
       label: lang.payment.status,
       value: 'status',
       setValue: string => `${tradeStatusOptions[string]}`
+    }, {
+      label: lang.payment.createAt,
+      value: 'createAt'
     }, {
       label: lang.payment.at,
       value: 'paymentAt'
